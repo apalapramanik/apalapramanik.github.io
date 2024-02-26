d9c8c512973ccabbed61c6b9613e9399884bb82b
apalapramanik <apalapramanik@github.com>
2024-02-26 09:49:24 -0600

    script.js added

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 /* script.js */
 console.log("Hello world!"); //this is executed first
 console.log("I'm doing JavaScript!"); //this is executed second
+console.log("I am APALA and this is my website")
