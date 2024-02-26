ac5bf690b8865c020fe61d07992e119b17c9103f
apalapramanik <apalapramanik@github.com>
2024-02-26 10:04:29 -0600

    script.js added

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ console.log("Hello world!"); //this is executed first
 console.log("I'm doing JavaScript!"); //this is executed second
 console.log("I am APALA and this is my website");
 
-let message = 'This is a string and next is my shoe size'; //a String
+let message = 'This is a string'; //a String
+console.log(message)
 console.log(typeof message); //=> `string`
+
 let shoeSize = 6; //a Number
+console.log(shoeSize)
 console.log(typeof shoeSize); //=> 'number'
